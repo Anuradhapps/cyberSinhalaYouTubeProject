4e964f89a5ca8bc5c6aea7a27ca2347a47bac89f
Anuradha_pps <anuradha.pps2024@gmail.com>
2024-06-11 16:17:27 +0530

    done

---
+++ resources/views/pages/home/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+<h1>Home1</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TodoController;
+use App\Models\Todo;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/',[HomeController::class,'index'])->name('home');
+Route::get('/todo',[TodoController::class,'index'])->name('todo');
